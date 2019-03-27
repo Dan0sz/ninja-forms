8d91f064691b62b243533d6ce6730cde961f3acb
Kyle Johnson <kyle@saturdaydrive.io>
2019-03-27 11:38:19 -0400

    Implement list options.

--- assets/js/builder/views/fields/preview/element.js
+++ assets/js/builder/views/fields/preview/element.js
@@ -49,7 +49,31 @@ define( [], function() {
 					// ...
 				},
 				renderOptions: function() {
-					// ...
+					console.log(this.type);
+					console.log(this.options);
+
+					switch(this.type) {
+						case 'liststate':
+						case 'listselect':
+						case 'listmultiselect':
+							var options = this.options.models.filter(function(option){
+								return option.get('selected');
+							});
+							if(0 == options.length) options = this.options.models;
+							return '<option>' + options[0].get('label') + '</option>';
+						case 'listcheckbox':
+							return this.options.models.reduce(function(html, option) {
+								return html += '<input type="checkbox"><label>' + option.get('label')  + '</label>';
+							}, '');
+						case 'listradio':
+							return this.options.models.reduce(function(html, option) {
+								return html += '<input type="radio"><label>' + option.get('label')  + '</label>';
+							}, '');
+						case 'listcountry':
+							return '<option>' + this.default + '</option>';
+						default:
+							return '';
+					}
 				},
 				renderOtherAttributes: function() {
 					// ...
