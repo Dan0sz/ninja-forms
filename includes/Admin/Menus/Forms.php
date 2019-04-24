29f96dcddc6f0a8fb5e01c7c9fbab239beea6ed6
Kyle Johnson <kyle@saturdaydrive.io>
2019-04-24 15:08:45 -0400

    Update builder_dev_mode default check.

--- includes/Admin/Menus/Forms.php
+++ includes/Admin/Menus/Forms.php
@@ -321,7 +321,8 @@ final class NF_Admin_Menus_Forms extends NF_Abstracts_Menu
         if(isset($_GET['nf_dev_mode']) && $_GET['nf_dev_mode']){
             $dev_mode = absint($_GET['nf_dev_mode']);
         } else {
-            $dev_mode = Ninja_Forms()->get_setting( 'builder_dev_mode', 1 );
+            $dev_mode = Ninja_Forms()->get_setting( 'builder_dev_mode' );
+            if(0 !== $dev_mode) $dev_mode = 1; // If not THE SAME AS 0, then 1.
         }
 
         wp_localize_script( 'nf-builder', 'nfAdmin', array(
