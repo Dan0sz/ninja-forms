25120ba8a95c33821311efff2aa852b8e5bcce03
KR Moorhouse <spkr4thded@gmail.com>
2019-04-26 15:30:08 +0000

    Added realistic field support for save progress button.

--- assets/js/builder/views/fields/fieldItem.js
+++ assets/js/builder/views/fields/fieldItem.js
@@ -42,6 +42,7 @@ define( ['views/app/itemControls', 'views/fields/preview/element', 'views/fields
 			if('liststate' == type) type = 'listselect';
 			if('listcountry' == type) type = 'listselect';
 			if('listmultiselect' == type) type = 'listselect';
+			if('save' == type) type = 'submit';
 
 			// Only show preview / realisitic fields when not `html`, `hidden`, `note`, or `recaptcha`.
 			var previewFieldTypeBlacklist = ['html', 'hidden', 'note', 'recaptcha'];
@@ -50,7 +51,7 @@ define( ['views/app/itemControls', 'views/fields/preview/element', 'views/fields
 				this.previewElement.show( new previewElementView( { model: this.model } ) );
 
 				// Only show the preview label when not `submit`, or `hr`.
-				var showLabelFieldTypeBlacklist = ['submit', 'hr'];
+				var showLabelFieldTypeBlacklist = ['submit', 'save', 'hr'];
 				if(-1 == showLabelFieldTypeBlacklist.indexOf(this.model.get('type'))) {
 					this.previewLabel.show( new previewLabelView( { model: this.model } ) );
 				}
